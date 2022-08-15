2738db600973bec11ff10170e66738257ab30562
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-15 17:28:46 +0900

    Add Welcome link.

--- pages/index.js
+++ pages/index.js
@@ -136,6 +136,7 @@ function App() {
         <header className="QiitaApp-header">
           <font color="red"><b>{error}</b></font><br />
           <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2021/08/25/224215" target="_blank" rel="noreferrer">Next.jsでQiitaAPIを使って記事情報を取得して表示(vercel-nextjs3)</a><br />
+          <a className="QiitaApp-link" href="welcome" target="_blank" rel="noreferrer">Welcome</a><br />
           <h3>QiitaでNext.jsタグありの記事を表示</h3>
           <br />
           <button onClick={() => {tagButtonClick("React")}}>React</button>
